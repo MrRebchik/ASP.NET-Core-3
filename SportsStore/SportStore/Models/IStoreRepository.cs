ba478df77e05d31334d120ac8fc4b9824ea54308
Penulltimo <38166546+MrRebchik@users.noreply.github.com>
2024-09-28 16:39:04 +0300

    Перевел на версию framework .netcore 3.1

--- SportsStore/SportStore/Models/IStoreRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Linq;
-
-namespace SportStore.Models
-{
-    public interface IStoreRepository
-    {
-        IQueryable<Product> Products { get; }
-    }
-}
---
+++ SportsStore/SportsSln/SportsStore/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SportsStore.Models
+{
+    public class Product
+    {
+        public long ProductId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        [Column(TypeName = "decimal(8,2)")]
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+
+    }
+}
