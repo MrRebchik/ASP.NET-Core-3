cef08b99186f94b70eb2c5ea8baaff301c02bf9d
Penulltimo <38166546+MrRebchik@users.noreply.github.com>
2024-10-28 21:59:42 +0300

    Добавлены стили и частичное представление продуктов

--- SportsStore/SportsSln/SportsStore/Infrastucture/PageLinkTagHelper.cs
+++ SportsStore/SportsSln/SportsStore/Infrastucture/PageLinkTagHelper.cs
@@ -20,6 +20,10 @@ namespace SportsStore.Infrastucture
         public ViewContext ViewContext { get; set; }
         public PagingInfo PageModel { get; set; }
         public string PageAction { get; set; }
+        public bool PageClassesEnabled { get; set; } = false;
+        public string PageClass { get; set; }
+        public string PageClassNormal { get; set; }
+        public string PageClassSelected { get; set; }
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             IUrlHelper urlHelper = urlHelperFactory.GetUrlHelper(ViewContext);
@@ -28,6 +32,11 @@ namespace SportsStore.Infrastucture
             {
                 TagBuilder tag = new TagBuilder("a");
                 tag.Attributes["href"] = urlHelper.Action(PageAction, new {productPage = i});
+                if (PageClassesEnabled)
+                {
+                    tag.AddCssClass(PageClass);
+                    tag.AddCssClass(i == PageModel.CurrentPage ? PageClassSelected : PageClassNormal);
+                }
                 tag.InnerHtml.Append(i.ToString());
                 result.InnerHtml.AppendHtml(tag);
             }
